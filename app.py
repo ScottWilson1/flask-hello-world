255349c210ea9ef36806cdffd00d857638144653
Scott Wilson <scottwilson84@gmail.com>
2025-03-31 03:45:19 +0000

    added db_test route

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import psycopg2
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World from Scott in 3308'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://scott_lab10_db_user:WgJgy2PuSOVpBbpA9fBzzfP2L2Usrf4K@dpg-cvl0sdre5dus73bup9qg-a/scott_lab10_db")
+    conn.close()
+    return "Database Connection Successful"
