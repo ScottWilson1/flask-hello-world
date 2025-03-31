1706cd10c0ed1ecfe2e35a88afb50b7f226cc8b3
Scott Wilson <scottwilson84@gmail.com>
2025-03-31 03:57:53 +0000

    added db_create route

--- app.py
+++ app.py
@@ -11,3 +11,21 @@ def db_test():
     conn = psycopg2.connect("postgresql://scott_lab10_db_user:WgJgy2PuSOVpBbpA9fBzzfP2L2Usrf4K@dpg-cvl0sdre5dus73bup9qg-a/scott_lab10_db")
     conn.close()
     return "Database Connection Successful"
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg2.connect('postgresql://scott_lab10_db_user:WgJgy2PuSOVpBbpA9fBzzfP2L2Usrf4K@dpg-cvl0sdre5dus73bup9qg-a/scott_lab10_db')
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball(
+            First varchar(225),
+            Last varchar(225),
+            City varchar(225),
+            Name varchar(225),
+            Number int
+        );
+        ''')
+    conn.commit()
+    conn.close()
+    return 'Basketball Table Successfully Created'
+
